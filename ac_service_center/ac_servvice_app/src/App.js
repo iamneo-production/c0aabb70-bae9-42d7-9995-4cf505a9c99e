8ab828a8c0b72c6820db97864a8104e6686757d4
preetham-m <m.preetham2019@gmail.com>
2022-09-27 19:28:58 +0000

    first commit(temp commit check)

--- ac_service_center/ac_servvice_app/src/App.js
+++ ac_service_center/ac_servvice_app/src/App.js
@@ -1,12 +1,26 @@
 import {Login} from "./pages/Login";
-function App() {
-  return (
-    <div className="App">
-      <Login>
-         
-      </Login>
-    </div>
-  );
+import React,{component} from "react"
+import { Form, Col, Button } from "react-bootstrap";
+class App extends component {
+  render() {ech
+    return (
+      	<Form>
+	        <Form.Row>
+		        <Col>
+		            <Form.Control type="email" placeholder="Enter email" />
+		        </Col>
+
+		        <Col>
+		            <Form.Control type="password" placeholder="Enter password" />
+		        </Col>
+
+		        <Col>
+		            <button>Submit</button>
+		        </Col>
+	        </Form.Row>
+      	</Form>
+    );
+  }
 }
 
 export default App;
--- ac_service_center/ac_servvice_app/src/pages/Login.jsx
+++ ac_service_center/ac_servvice_app/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-/*export const Login = () => {
+export const Login = () => {
     return ("hello!")
-} */
+} 
 
